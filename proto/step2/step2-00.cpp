30b8e08553313d81f7570764250d432ece709f52
Joel Falcou <joel.falcou@lri.fr>
2011-05-10 20:55:32 -0500

    Updated Step 2 base files

--- proto/step2/step2-00.cpp
+++ proto/step2/step2-00.cpp
@@ -2,7 +2,7 @@
  *           Getting Jiggy with Boost.Proto - Boost'Con 2011 Tutorial
  *                                 by Joel Falcou
  *
- *                      Step 1 : Putting Expressions to Work
+ *                             Step 2 : Transformers !
  *
  *             Distributed under the Boost Software License, Version 1.0.
  *                    See accompanying file LICENSE.txt or copy at
@@ -15,15 +15,29 @@
 
 /*******************************************************************************
  * our EDSL has now been refurbished and split into convenient files.
- ******************************************************************************/
-
-/*******************************************************************************
- * Now test our analytical function computation systems
+ *
+ * Your task is this final settings is to define trasnforms and functions to be
+ * able to compute, at compile-time, the analytical derivatives of any of our
+ * analytical functions.
+ *
+ * Here is a plan on how to tackle this:
+ *  - take a piece of paper and think about these derivation rules learned in
+ *  high school. How do they relate to the original functions ? Can you spot
+ *  why we dont need anythine more than a trasnform ?
+ *  - There is special cases in derivatives rules. What are they and how can we
+ *  capture them ?
+ *  - All in all, what are we needing as a basic proto trasnform ?
+ *  - Now, what's a Nth degree derivative ? Do we need extra work ?
+ *  - Same question for partial derivatives ? What do we need ?
+ *
+ *  The following hour or so is here for you to think about these questions and
+ *  try to answer them and implement something usign Proto. Don't worry, if
+ *  you lay down everything elegantly, this is a mere 50 lines you'll need to
+ *  write.
+ *
+ *  Don't forget to tests corner cases and see for yourself :)
  ******************************************************************************/
 
 int main()
 {
-  std::cout << (cos(_x*3.14159))(0.25) << "\n";
-  std::cout << (cos(_x)*cos(_x) + sin(_x)*sin(_x))(1.23456) << "\n";
-
 }
